8f395ea9a5f20692f92285b88b8d366052df11a0
g-edgah <172597903+g-edgah@users.noreply.github.com>
2025-12-07 01:21:44 +0400

    auth

--- server/src/controller/auth.js
+++ server/src/controller/auth.js
@@ -2,6 +2,39 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
-export const register = async () => {
-    boom
+export const register = async (req, res) => {
+    try {
+        const {
+            firstName, 
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occcupation 
+
+        } = req.body
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            firstName, 
+            lastName,
+            email,
+            password: passwordHash,
+            picturePath,
+            friends,
+            location,
+            occcupation,
+            viewedProfile: Math.floor(Math.random()*10000), //dummy value
+            impressions: Math.floor(Math.random()*10000), //dummy value
+        });
+
+        const savedUser = await newUser.save()
+
+    } catch (error) {
+        console.log("error in register function: "+error)
+    }
 }
